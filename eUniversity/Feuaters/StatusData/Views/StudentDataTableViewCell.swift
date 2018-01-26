a5bffd5b62aab27674ba99428bf721f45e876aab
Damir Ramic <ramicdamir@yahoo.com>
2018-01-26 08:01:54 +0100

    SV ProgressHUD and logindesign

---
+++ eUniversity/Feuaters/StatusData/Views/StudentDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentDataTableViewCell.swift
+//  eUniversity
+//
+//  Created by Damir Ramic on 25/01/2018.
+//  Copyright © 2018 Damir Ramich. All rights reserved.
+//
+
+import UIKit
+
+class StudentDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
